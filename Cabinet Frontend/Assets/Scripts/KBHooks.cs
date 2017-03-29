fcc098a1741dad11983e5b3085f8c955b726b9ca
Ryan O'Malley <ryeomalley@gmail.com>
2017-03-29 05:56:13 -0400

    Moved over files from previous repo.
    The only new thing different from the other repo is the CabinetUIShim, which is an AutoHotKey script that waits on the running process to close before closing itself. It also allows you to kill the currently running program with a button on the cabinet (which is the actual reason why it exists).

---
+++ Cabinet Frontend/Assets/Scripts/KBHooks.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class KBHooks : MonoBehaviour
+{
+    [DllImport("user32")]
+    protected static extern IntPtr SetWindowsHookEx(
+        HookType code, HookProc func, IntPtr hInstance, int threadID);
+
+    [DllImport("user32")]
+    protected static extern int UnhookWindowsHookEx(
+        IntPtr hhook);
+
+    [DllImport("user32")]
+    protected static extern int CallNextHookEx(
+        IntPtr hhook, int code, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("Kernel32", EntryPoint = "GetCurrentThreadId", ExactSpelling = true)]
+    public static extern Int32 GetCurrentWin32ThreadId();
+
+    // Hook types. To hook the keyboard we only need WH_KEYBOARD
+    protected enum HookType : int
+    {
+        WH_JOURNALRECORD = 0,
+        WH_JOURNALPLAYBACK = 1,
+        WH_KEYBOARD = 2,
+        WH_GETMESSAGE = 3,
+        WH_CALLWNDPROC = 4,
+        WH_CBT = 5,
+        WH_SYSMSGFILTER = 6,
+        WH_MOUSE = 7,
+        WH_HARDWARE = 8,
+        WH_DEBUG = 9,
+        WH_SHELL = 10,
+        WH_FOREGROUNDIDLE = 11,
+        WH_CALLWNDPROCRET = 12,
+        WH_KEYBOARD_LL = 13,
+        WH_MOUSE_LL = 14
+    }
+
+    protected IntPtr m_hhook = IntPtr.Zero;
+    protected HookType m_hookType = HookType.WH_KEYBOARD;
+
+    protected delegate int HookProc(int code, IntPtr wParam, IntPtr lParam);
+
+    //We install the hook and hold on to the hook handle.
+    //The handle will be need to unhook. 
+    protected bool Install(HookProc cbFunc)
+    {
+        if (m_hhook == IntPtr.Zero)
+            m_hhook = SetWindowsHookEx(
+                m_hookType,
+                cbFunc,
+                IntPtr.Zero,
+                (int)GetCurrentWin32ThreadId());
+
+        if (m_hhook == IntPtr.Zero)
+            return false;
+
+        return true;
+    }
+
+    protected void Uninstall()
+    {
+        if (m_hhook != IntPtr.Zero)
+        {
+            UnhookWindowsHookEx(m_hhook);
+            m_hhook = IntPtr.Zero;
+        }
+    }
+
+    protected int CoreHookProc(int code, IntPtr wParam, IntPtr lParam)
+    {
+        if (code < 0)
+            return CallNextHookEx(m_hhook, code, wParam, lParam);
+
+        Debug.Log(
+            "hook code =" + code.ToString() +
+            " lparam=" + lParam.ToString() +
+            " wparam=" + wParam.ToString());
+
+        // Yield to the next hook in the chain
+        return CallNextHookEx(m_hhook, code, wParam, lParam);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("install hook");
+        Install(CoreHookProc);
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Uninstall hook");
+        Uninstall();
+    }
+
+}
---
+++ Cabinet Frontend/Assets/Scripts/LaunchExternalProgram.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class LaunchExternalProgram : MonoBehaviour {
+
+    Process launchedGame;
+
+    // Use this for initialization
+    void Start () {
+        launchedGame = new Process();
+        launchedGame.StartInfo.FileName = "notepad.exe";
+        launchedGame.StartInfo.Arguments = "";
+
+        //use to create no window when running cmd script
+        launchedGame.StartInfo.UseShellExecute = true;
+        launchedGame.StartInfo.CreateNoWindow = true;
+        launchedGame.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+
+        Application.runInBackground = true;
+
+        launchedGame.Start();
+
+        //if you want program to halt until script is finished
+        //process.WaitForExit();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("q"))
+        {
+            launchedGame.Kill();
+            UnityEngine.Debug.Log("Killed");
+        }
+            
+	}
+}
